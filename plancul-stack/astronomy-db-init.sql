7dbae94dd1ab97d928f74933d2d5ff5b7cbcef22
cghislai <charlyghislain@gmail.com>
2018-12-11 15:28:22 +0200

    fix stack

---
+++ plancul-stack/astronomy-db-init.sql
@@ -0,0 +1,4 @@
+create database if not exists astronomy;
+grant all privileges on astronomy.* to 'plancul'@'%' identified by 'db-password';
+flush privileges;
+
--- plancul-stack/post-boot.sh
+++ plancul-stack/post-boot.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 create-jdbc-connection-pool --datasourceclassname org.mariadb.jdbc.MySQLDataSource --restype javax.sql.DataSource --property User="plancul":DatabaseName="plancul":Password="'db-password'":ServerName="db" plancul-db-pool
+create-jdbc-connection-pool --datasourceclassname org.mariadb.jdbc.MySQLDataSource --restype javax.sql.DataSource --property User="plancul":DatabaseName="astronomy":Password="'db-password'":ServerName="db" astronomy-db-pool
 
 create-jdbc-resource --connectionpoolid plancul-db-pool jdbc/plancul-db
+create-jdbc-resource --connectionpoolid astronomy-db-pool jdbc/plancul-astronomy-db
 
 create-javamail-resource  --mailhost postfix --fromaddress plancul@charlyghislain.com --mailuser plancul mail/plancul-mail
 
